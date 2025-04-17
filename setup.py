9f03f5a81c718a4553d6f64e6c5fd5583e785569
Anton Kovalevich <kai3341@gmail.com>
2025-04-17 17:05:15 +0200

    Disable mypyc

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import setup
 
 from setup_constants import DESCRIPTION, NAME, NAME_CANONICAL, VERSION
 
-if "NO_MYPYC" in environ:
-    ext_modules = []
-else:
+if "USE_MYPYC" in environ:
     from setup_mypycify import ext_modules
+else:
+    ext_modules = []
 
 
 requirements = []
