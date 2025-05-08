8ef5baa3ae542b8de971a99c24f0991e97db7af0
Anton Kovalevich <kai3341@gmail.com>
2025-05-08 03:36:26 +0200

    Apply dirty hack

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from os import environ
+from os import getenv
 
 from setuptools import setup
 
 from setup_constants import DESCRIPTION, NAME, NAME_CANONICAL, VERSION
 
-if "NO_MYPYC" not in environ:
+if getenv("USE_MYPYC") == "1":
     from setup_mypycify import ext_modules
 else:
     ext_modules = []
