2786dcc8a11283e026fbb1adf141e36af35e2964
Anton Kovalevich <kai3341@gmail.com>
2025-05-10 15:21:59 +0200

    Fix build warnings, bump version

--- setup.py
+++ setup.py
@@ -4,14 +4,11 @@ from os import getenv
 from setuptools import setup
 from wheel.bdist_wheel import bdist_wheel
 
-from setup_constants import DESCRIPTION, NAME, NAME_CANONICAL, VERSION
-
-requirements_build: list[str] = []
+from setup_constants import NAME
 
 if getenv("USE_MYPYC"):
     from setup_mypycify import ext_modules
 
-    requirements_build.append("mypyc")
 else:
     ext_modules = []
 
@@ -60,43 +57,6 @@ package_data = {
     NAME: ["py.typed"],
 }
 
-keywords = [
-    "asyncio",
-    "graceful",
-    "init",
-    "initialization",
-    "shutdown",
-    "manager",
-]
-
-classifiers__python_versions = (
-    "3 :: Only",
-    "3.9",
-    "3.10",
-    "3.11",
-    "3.12",
-    "3.13",
-    "3.14",
-)
-
-classifiers__programming_language = tuple(
-    f"Programming Language :: Python :: {i}"
-    # ===
-    for i in classifiers__python_versions
-)
-
-classifiers = [
-    "Development Status :: 3 - Alpha",
-    "Intended Audience :: Developers",
-    "License :: OSI Approved :: MIT License",
-    *classifiers__programming_language,
-    # Topic
-    "Topic :: Software Development :: Libraries",
-    "Topic :: Software Development :: Libraries :: Application Frameworks",
-    "Topic :: Software Development :: Libraries :: Python Modules",
-    # Typing
-    "Typing :: Typed",
-]
 
 py_modules = [
     NAME,
@@ -108,24 +68,15 @@ with open("README.md", encoding="utf-8") as readme_file:  # noqa: PTH123
 
 
 setup(
-    classifiers=classifiers,
-    description=DESCRIPTION,
     install_requires=requirements,
-    extras_require={
-        "dev": requirements_build,
-    },
     cmdclass={"bdist_wheel": BDistWheel},
-    license="MIT",
     packages=packages,
     package_data=package_data,
     long_description=long_description,
     long_description_content_type="text/markdown",
     include_package_data=True,
-    keywords=keywords,
-    name=NAME_CANONICAL,
     py_modules=py_modules,
     ext_modules=ext_modules,
     url="https://github.com/kai3341/systemPY",
-    version=VERSION,
     zip_safe=True,
 )
